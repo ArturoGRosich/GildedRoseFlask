1f83f188e128835adfefa55b223d3b49d2701d6c
Arturo Garrido Rosich <agarridorosich@cifpfbmoll.eu>
2022-03-16 19:53:03 +0100

    refactor: :fire: deleting useless imports

--- api.py
+++ api.py
@@ -2,10 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from resources.index import Index
 from resources.stock import Stock
-from resources.getByName import GetByName
-from resources.getByQuality import GetByQuality
-from resources.getBySellIn import GetBySellIn
-
 app = Flask(__name__)
 api = Api(app, catch_all_404s=True)
 
@@ -15,3 +11,4 @@ api.add_resource(Stock, "/items")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+    
